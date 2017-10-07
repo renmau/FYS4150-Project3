07a7a5a9e80eab47af386a62932f50d416661dbf
sefthus <elisabeth.strom95@gmail.com>
2017-10-07 12:39:58 +0200

    added functions
    added functions in main.cpp and mercury plot in plot_planets.py

--- ClassStuff/main.cpp
+++ ClassStuff/main.cpp
@@ -10,42 +10,49 @@
 
 using namespace std;
 
-int main(int numArguments, char **arguments)
-{
-    char *outfilename;
-    outfilename = arguments[1];
-    double years = 5.0;
-    int numTimesteps = years*100;
-    double dt = years / (numTimesteps-1);
-    if(numArguments >= 3) numTimesteps = atoi(arguments[2]);
+SolarSystem TwoBodyProblem(){
+    SolarSystem solarSystem;
+    // The Sun
+    solarSystem.createCelestialBody( vec3(0, 0, 0), vec3( 0, 0, 0), 1.0 );
+    //The Earth
+    solarSystem.createCelestialBody( vec3(1, 0, 0), vec3( 0, 2*M_PI, 0 ), 3e-6 );
+    return solarSystem;
 
+}
+SolarSystem ThreeBodyProblem(bool fixed_CM){
+    vec3 vJ(0, sqrt(4*M_PI*M_PI/5.2), 0);
+
+    //For unmoving center-of-mass of three body system jupiter,earth,sun
+    if (fixed_CM == true){
+        SolarSystem solarSystem;
+        double mE = 3e-6;
+        double mJ = 9.5e-4;
+        vec3 vE(0, sqrt(4*M_PI*M_PI), 0);
+
+        vec3 vSun=-mE*vE - mJ*vJ;
+        vec3 CM = (vec3(1, 0, 0)*mE + vec3(5.2,0,0)*mJ)/(mE+mJ+1.);
+
+        // The Sun
+        solarSystem.createCelestialBody( vec3(0, 0, 0)-CM, vSun, 1.0 );
+        //The Earth
+        solarSystem.createCelestialBody( vec3(1, 0, 0)-CM, vE, 3e-6 );
+        // Jupiter
+        solarSystem.createCelestialBody( vec3(5.2,0,0)-CM, vJ, 9.5e-4);
+        return solarSystem;
+
+    }
+    // Three body problem without correctionfor center-of-mass
+    else{
+        SolarSystem solarSystem = TwoBodyProblem();
+        // Jupiter
+        solarSystem.createCelestialBody( vec3(5.2,0,0), vJ, 9.5e-4);
+        return solarSystem;
+    }
+}
+
+SolarSystem FullSystem(){
 
     SolarSystem solarSystem;
-    // We create new bodies like this. Note that the createCelestialBody function returns a reference to the newly created body
-    // This can then be used to modify properties or print properties of the body if desired
-    // Use with: solarSystem.createCelestialBody( position, velocity, mass );
-
-    //For center-of-mass of three body system jupiter,earth,sun
-    /*
-    double mE = 3e-6;
-    double mJ = 9.5e-4;
-    vec3 vE(0,sqrt(4*M_PI*M_PI),0);
-    vec3 vJ(0,sqrt(4*M_PI*M_PI/5.2),0);
-    vec3 vSun=-mE*vE - mJ*vJ;
-    vec3 CM = (vec3(1, 0, 0)*mE + vec3(5.2,0,0)*mJ)/(mE+mJ);
-    vec3 rE = vec3(1, 0, 0)-CM;
-    vec3 rJ = vec3(5.2,0,0)-CM;
-    vec3 rSun = vec3(0, 0, 0)-CM;
-    // The Sun
-    solarSystem.createCelestialBody( rSun, vSun, 1.0 );
-    //The Earth
-    solarSystem.createCelestialBody( rE, vE, 3e-6 );
-    // Jupiter
-    solarSystem.createCelestialBody( rJ, vJ, 9.5e-4);
-    */
-
-    // full solar system:
-    /*
     // The Sun
     solarSystem.createCelestialBody( vec3(2.280645281880971E-03,5.669034916996534E-03,-1.316063812538178E-04), vec3(-5.124710550422158E-06*365., 5.551631297499966E-06*365., 1.189826204505761E-07*365.), 1.0 );
     //The Earth
@@ -66,18 +73,42 @@ int main(int numArguments, char **arguments)
     solarSystem.createCelestialBody( vec3(1.787905976395623E+01,8.773022260100626,-1.990427195843487E-01), vec3(-1.761322338307447E-03,3.347557466700209E-03,3.516031928207541E-05)*365., 4.4e-5);
     // Pluto
     solarSystem.createCelestialBody( vec3(1.051352392432785E+01,-3.171624029129188E+01,3.527021366368159E-01), vec3(3.048301492667712E-03,3.404636168561237E-04,-9.058944090468905E-04)*365., 6.55e-9);
-    */
 
+    return solarSystem;
+}
+
+SolarSystem MercuryPerihelionPrecession(){
     // For Mercury perihelion precession:
+    SolarSystem solarSystem;
+
     double mM = 1.65e-7;
     vec3 vM(0,12.44,0);
     vec3 rM = vec3(0.3075, 0, 0);
     vec3 rSun = vec3(0, 0, 0);
-    vec3 vSun = vec3(0,0,0);
+    //vec3 vSun = vec3(0,0,0);
+    vec3 vSun=-mM*vM;
+    vec3 CM = (rM*mM)/(mM+1.);
     // The Sun
-    solarSystem.createCelestialBody( rSun, vSun, 1.0 );
+    solarSystem.createCelestialBody( rSun-CM, vSun, 1.0 );
     //Mercury
-    solarSystem.createCelestialBody( rM, vM, mM );
+    solarSystem.createCelestialBody( rM-CM, vM, mM );
+
+    return solarSystem;
+}
+int main(int numArguments, char **arguments)
+{
+    char *outfilename;
+    outfilename = arguments[1];
+    double years = 50.0;
+    int numTimesteps = years*100;
+    double dt = years / (numTimesteps-1);
+    if(numArguments >= 3) numTimesteps = atoi(arguments[2]);
+    bool fixed_CM = true;
+
+    //SolarSystem solarSystem = TwoBodyProblem();
+    //SolarSystem solarSystem = ThreeBodyProblem(fixed_CM);
+    //SolarSystem solarSystem = FullSystem();
+    SolarSystem solarSystem = MercuryPerihelionPrecession();
 
 
     // To get a list (a reference, not copy) of all the bodies in the solar system, we use the .bodies() function
--- plot_planets.py
+++ plot_planets.py
@@ -15,20 +15,41 @@ def plot_formatting(fam='serif',fam_font='Computer Modern Roman',font_size=14,ti
 	plt.xticks(fontsize=tick_size)
 	plt.yticks(fontsize=tick_size)
 
-def plot_planet_oribt(x,y,planet='planet',title_label='some title'):
+def plot_planet_oribt(x,y,planet_label='_nolegend_',title_label=' ',hold=False):
 	#plot_formatting()
-	if isinstance(x[0,:],np.ndarray)==True: # if x is a array of arrays
-		[plt.plot(x[i],y[i],label=planet[i]) for i in range(len(x[:,0]))]
-	else: plt.plot(x,y,label=planet)
+	if isinstance(x[0],np.ndarray)==True: # if x is a array of arrays
+		if planet_label =='_nolegend_': planet_label=['_nolegend_' for i in range(len(x[:,0]))]
+		[plt.plot(x[i], y[i], label=planet_label[i], ) for i in range(len(x[:,0]))]
+		#if planet_label !='_nolegend_': plt.legend()
+	else: 
+		plt.plot(x,y,label=planet_label)
 	plt.axis('equal')
-	plt.legend()
 	plt.xlabel('x')
 	plt.ylabel('y(x)')
 	plt.title(title_label)
-	plt.show()
+	plt.tight_layout()
+	if hold !=True: 
+		plt.legend()
+		plt.show()
+
+
+def two_body_problem():
+	N1 = np.genfromtxt('exercice_3b_se_euler.txt')
+	N2 = np.genfromtxt('exercice_3b_se_verlet.txt')  	 
+	x_sun_e, y_sun_e, z_sun_e 			= N1[:,0], N1[:,1], N1[:,2]						 							 
+	x_earth_e, y_earth_e, z_earth_e 	= N1[:,3], N1[:,4], N1[:,5]
+
+	x_sun_v, y_sun_v, z_sun_v 			= N2[:,0], N2[:,1], N2[:,2]						 							 
+	x_earth_v, y_earth_v, z_earth_v 	= N2[:,3], N2[:,4], N2[:,5]
+
+	x_planet = np.array([x_sun_e, x_earth_e, x_sun_v, x_earth_v])
+	y_planet = np.array([y_sun_e, y_earth_e, y_sun_v, y_earth_v])
+	planet_label = ['_nolegend_','Earth Euler','_nolegend_','Earth V-Verlet']
+	plot_planet_oribt(x_planet, y_planet, planet_label,'Two-body-problem Sun-Earth')
 
 def three_body_problem():
-	N = np.genfromtxt('exercice_3f_je.txt') 	 
+	N = np.genfromtxt('exercice_3f_je.txt')
+	N = np.genfromtxt('exercice_3e.txt')
 	x_sun, y_sun, z_sun 			= N[:,0], N[:,1], N[:,2]						 							 
 	x_earth, y_earth, z_earth 		= N[:,3], N[:,4], N[:,5]
 	x_jupiter, y_jupiter, z_jupiter = N[:,6], N[:,7], N[:,8]
@@ -36,46 +57,47 @@ def three_body_problem():
 	x_planet = np.array([x_sun, x_earth, x_jupiter])
 	y_planet = np.array([y_sun, y_earth, y_jupiter])
 	planet_label = ['Sun','Earth','Jupiter']
-	plot_planet_oribt(x_planet,y_planet,planet_label,'Three-body-problem Sun-Earth-Jupiter')
+	plot_planet_oribt(x_planet, y_planet, planet_label,'Three-body-problem Sun-Earth-Jupiter')
 
 def full_system():# entire classical solar system
-	N = np.genfromtxt('exercice_3f_full_system.txt') 	
+	N = np.genfromtxt('exercice_3f_full_system.txt') 
+
 	x_sun, y_sun, z_sun 			= N[:,0], N[:,1], N[:,2]						 							 
 	x_earth, y_earth, z_earth 		= N[:,3], N[:,4], N[:,5]
 
 	x_jupiter, y_jupiter, z_jupiter = N[:,6], N[:,7], N[:,8]
 	x_jupiter, y_jupiter, z_jupiter = N[:,6], N[:,7], N[:,8]
-	x_mercury, y_mercury, z_mercury = N[:,9],N[:,10],N[:,11]
-	x_venus, y_venus, z_venus 		= N[:,12],N[:,13],N[:,14]
-	x_mars, y_mars, z_mars 			= N[:,15],N[:,16],N[:,17]
-	x_saturn, y_saturn, z_saturn 	= N[:,18],N[:,19],N[:,20]
-	x_neptune, y_neptune, z_neptune = N[:,21],N[:,22],N[:,23]
-	x_uranus, y_uranus, z_uranus 	= N[:,24],N[:,25],N[:,26]
-	x_pluto, y_pluto, z_pluto 		= N[:,27],N[:,28],N[:,29]
+	x_mercury, y_mercury, z_mercury = N[:,9], N[:,10], N[:,11]
+	x_venus, y_venus, z_venus 		= N[:,12], N[:,13], N[:,14]
+	x_mars, y_mars, z_mars 			= N[:,15], N[:,16], N[:,17]
+	x_saturn, y_saturn, z_saturn 	= N[:,18], N[:,19], N[:,20]
+	# fix uranus before neptune here and in c++
+	x_neptune, y_neptune, z_neptune = N[:,21], N[:,22], N[:,23]
+	x_uranus, y_uranus, z_uranus 	= N[:,24], N[:,25], N[:,26]
+	x_pluto, y_pluto, z_pluto 		= N[:,27], N[:,28], N[:,29]
 
-	x_planet=np.array([x_sun,x_mercury,x_venus,x_earth,x_mars,x_jupiter,x_saturn,x_neptune,x_uranus,x_pluto])
-	y_planet=np.array([y_sun,y_mercury,y_venus,y_earth,y_mars,y_jupiter,y_saturn,y_neptune,y_uranus,y_pluto])
-	planet_label = ['Sun','Mercury','Venus','Earth','Mars','Jupiter','Saturn','Neptune','Uranus','Pluto']
+	x_planet=np.array([x_sun,x_mercury,x_venus,x_earth,x_mars,x_jupiter,x_saturn,x_uranus,x_neptune,x_pluto])
+	y_planet=np.array([y_sun,y_mercury,y_venus,y_earth,y_mars,y_jupiter,y_saturn,y_uranus,y_neptune,y_pluto])
+	planet_label = ['Sun','Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
 
-	plot_planet_oribt(x_planet,y_planet,planet_label,'The classic solar system')
+	plot_planet_oribt(x_planet, y_planet, planet_label,'The classic solar system')
+
+def mercury_perihelion():
+	N = np.genfromtxt('exercice_3g_mercury.txt') 
+
+	x_sun, y_sun, z_sun 			= N[:,0], N[:,1], N[:,2]						 							 
+	x_mercury, y_mercury, z_mercury	= N[:,3], N[:,4], N[:,5]
+
+	x_planet=np.array([x_sun,x_mercury])
+	y_planet=np.array([y_sun,y_mercury])
+	planet_label = ['Sun','Mercury']
+
+	plot_planet_oribt(x_mercury, y_mercury, 'Mercury','The Mercury orbit',hold=True)
+	plt.plot(x_sun,y_sun,'rx',label='Sun')
+	plt.legend()
+	plt.show()
+
+#two_body_problem()
 three_body_problem()
-full_system()
-'''
-plt.plot(x_sun,y_sun,label='sun')
-plt.plot(x_mercury,y_mercury,label='mercury')
-plt.plot(x_venus,y_venus,label='venus')
-plt.plot(x_earth,y_earth,label='earth')
-plt.plot(x_mars,y_mars,label='mars')
-
-plt.plot(x_jupiter,y_jupiter,label='jupiter')
-plt.plot(x_saturn,y_saturn,label='saturn')
-plt.plot(x_neptune,y_neptune,label='neptune')
-plt.plot(x_uranus,y_uranus,label='uranus')
-plt.plot(x_pluto,y_pluto, label='pluto')
-plt.axis('equal')
-plt.legend()
-plt.xlabel('x')
-plt.ylabel('y(x)')
-#plt.title('Change in gravitational force')
-#plt.legend(['analytical','numerical'])
-plt.show()'''
+#full_system()
+#mercury_perihelion()
